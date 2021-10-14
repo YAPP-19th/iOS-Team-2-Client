cedf7e29978a7dd3d78faaa175e47e82914527ce
IT Learning <yo7504@kakao.com>
2021-10-14 19:53:14 +0900

    3,4,5,6 셀 제작 (#10)
    * README 수정 (#4)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: IT Learning <yo7504@kakao.com>
    Co-authored-by: dochoi <ehdrb345@naver.com>
    
    * Update
    
    * Cell 제작
    
    * 수정사항 반영
    
    Co-authored-by: sanghee <61302874+sanghee-dev@users.noreply.github.com>
    Co-authored-by: dochoi <ehdrb345@naver.com>

---
+++ Budi/Budi/Source/TeamRecruitment/Cells/ProjectNameCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectNameCollectionViewCell.swift
+//  Budi
+//
+//  Created by ITlearning on 2021/10/13.
+//
+
+import UIKit
+
+class ProjectNameCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Budi/Budi/Source/TeamRecruitment/TeamRecruitmentWritingViewController.swift
+++ Budi/Budi/Source/TeamRecruitment/TeamRecruitmentWritingViewController.swift
@@ -12,13 +12,26 @@ final class TeamRecruitmentWritingViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
         configureNavigationBar()
+        configureCells()
+    }
+    func configureCells() {
+        let calender = UINib(nibName: "CalendarCollectionViewCell", bundle: nil)
+        let selectPhoto = UINib(nibName: "SelectPhotoCollectionViewCell", bundle: nil)
+        let projectName = UINib(nibName: "ProjectNameCollectionViewCell", bundle: nil)
+        let location = UINib(nibName: "LocationCollectionViewCell", bundle: nil)
+        collectionView.register(calender, forCellWithReuseIdentifier: "calendarCell")
+        collectionView.register(selectPhoto, forCellWithReuseIdentifier: "selectPhotoCell")
+        collectionView.register(projectName, forCellWithReuseIdentifier: "projectNameCell")
+        collectionView.register(location, forCellWithReuseIdentifier: "locationCell")
     }
 }
 
 extension TeamRecruitmentWritingViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 4
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {

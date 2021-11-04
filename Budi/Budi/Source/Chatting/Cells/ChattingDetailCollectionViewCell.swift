2ee45e213db102eb8abd279281f8f6c42cbaf0c1
sanghee-dev <leeesangheee@gmail.com>
2021-11-04 21:00:44 +0900

    #17 채팅 상세 화면 메세지 셀 추가

---
+++ Budi/Budi/Source/Chatting/Cells/ChattingDetailCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ChattingDetailCollectionViewCell.swift
+//  Budi
+//
+//  Created by leeesangheee on 2021/11/04.
+//
+
+import UIKit
+
+class ChattingDetailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- Budi/Budi/Source/Chatting/ChattingDetailViewController.swift
+++ Budi/Budi/Source/Chatting/ChattingDetailViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ChattingDetailViewController: UIViewController {
 
+    @IBOutlet weak var collecitonView: UICollectionView!
     weak var coordinator: ChattingCoordinator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
+        configureCollectionView()
+    }
+}
+
+private extension ChattingDetailViewController {
+    func configureCollectionView() {
+        collecitonView.dataSource = self
+        collecitonView.delegate = self
+        collecitonView.register(.init(nibName: ChattingDetailCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: ChattingDetailCollectionViewCell.identifier)
+        collecitonView.backgroundColor = .systemGroupedBackground
+    }
+}
+
+extension ChattingDetailViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ChattingDetailCollectionViewCell.identifier, for: indexPath) as UICollectionViewCell
+        return cell
+    }
+}
+
+extension ChattingDetailViewController: UICollectionViewDelegate {
+
+}
+
+extension ChattingDetailViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: 100)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        8
     }
 }
 

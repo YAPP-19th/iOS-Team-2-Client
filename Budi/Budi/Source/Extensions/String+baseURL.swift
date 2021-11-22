8828be43cf8a743f852e2f79b1ed803c45a39c77
dochoi <dochoi@student.42seoul.kr>
2021-11-23 00:56:40 +0900

    HomeViewModel 생성
    * 킹피셔 추가
    * Combine Cocoa 추가
    * MOYA 추가
    * 다크모드 막기
    * API 작업 추가

---
+++ Budi/Budi/Source/Extensions/String+baseURL.swift
@@ -0,0 +1,12 @@
+//
+//  String+baseURL.swift
+//  Budi
+//
+//  Created by 최동규 on 2021/11/22.
+//
+
+import Foundation
+
+extension String {
+    static var baseURLString: String = "http://3.37.75.75:8080/api/v1"
+}

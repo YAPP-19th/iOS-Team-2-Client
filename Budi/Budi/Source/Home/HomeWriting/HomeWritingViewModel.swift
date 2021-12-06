f94078cce1932265a3b7f05b9bdf7168e8668c98
sanghee-dev <leeesangheee@gmail.com>
2021-12-07 00:09:21 +0900

    YAPP-19th#32 홈부분 폴더 정리 및 게시글 생성 뷰모델 초기구성

---
+++ Budi/Budi/Source/Home/HomeWriting/HomeWritingViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  HomeWritingViewModel.swift
+//  Budi
+//
+//  Created by leeesangheee on 2021/12/07.
+//
+
+import Combine
+import Foundation
+import Moya
+
+final class HomeWritingViewModel: ViewModel {
+    
+    struct Action {
+        let fetch = PassthroughSubject<Void, Never>()
+        let refresh = PassthroughSubject<Void, Never>()
+    }
+
+    struct State {
+        let post = CurrentValueSubject<Post?, Never>(nil)
+    }
+    
+    let action = Action()
+    let state = State()
+    private var cancellables = Set<AnyCancellable>()
+    private let provider = MoyaProvider<PostTarget>()
+    
+    init() {
+        action.fetch
+            .sink(receiveValue: { [weak self] _ in
+                guard let self = self else { return }
+
+            }).store(in: &cancellables)
+
+        action.refresh
+            .sink { [weak self] _ in
+                self?.action.fetch.send(())
+            }.store(in: &cancellables)
+
+        action.fetch.send(())
+    }
+}

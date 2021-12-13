adf13a98584d41662b78859438a32b44ba09c2fa
sanghee-dev <leeesangheee@gmail.com>
2021-12-13 18:43:34 +0900

    #31 색상 추가 및 수정

--- Budi/Budi/Source/Extensions/UIColor.swift
+++ Budi/Budi/Source/Extensions/UIColor.swift
@@ -9,30 +9,38 @@ import UIKit
 
 extension UIColor {
     static var budiGreen: UIColor {
-        UIColor(red: 0.41, green: 0.86, blue: 0.67, alpha: 1.00)
+        UIColor(red: 0.41, green: 0.86, blue: 0.67, alpha: 1)
     }
 
     static var budiLightGreen: UIColor {
-        UIColor.budiGreen.withAlphaComponent(0.2)
+        UIColor(red: 0.701, green: 0.882, blue: 0.803, alpha: 1)
+    }
+    
+    static var budiBackgroundGreen: UIColor {
+        UIColor(red: 0.941, green: 0.984, blue: 0.968, alpha: 1)
     }
 
     static var budiRed: UIColor {
-        UIColor(red: 0.85, green: 0.12, blue: 0.16, alpha: 1.00)
+        UIColor(red: 0.85, green: 0.12, blue: 0.16, alpha: 1)
     }
 
     static var budiBlack: UIColor {
-        UIColor(red: 0.117, green: 0.117, blue: 0.117, alpha: 1.000)
+        UIColor(red: 0.117, green: 0.117, blue: 0.117, alpha: 1)
     }
 
     static var budiDarkGray: UIColor {
-        UIColor(red: 0.360, green: 0.360, blue: 0.360, alpha: 1.000)
+        UIColor(red: 0.36, green: 0.36, blue: 0.36, alpha: 1)
     }
 
     static var budiGray: UIColor {
-        UIColor(red: 0.556, green: 0.556, blue: 0.556, alpha: 1.000)
+        UIColor(red: 0.556, green: 0.556, blue: 0.556, alpha: 1)
     }
 
     static var budiLightGray: UIColor {
-        UIColor(red: 0.866, green: 0.866, blue: 0.866, alpha: 1.000)
+        UIColor(red: 0.866, green: 0.866, blue: 0.866, alpha: 1)
+    }
+    
+    static var budiTransparentGray: UIColor {
+        UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
     }
 }

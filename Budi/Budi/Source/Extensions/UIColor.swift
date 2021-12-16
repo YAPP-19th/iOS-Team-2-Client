55f8031ab8e634026a86d54a75a0eeede35303ad
sanghee-dev <leeesangheee@gmail.com>
2021-12-16 22:14:49 +0900

    #36 앱 사용 색상들 추가

--- Budi/Budi/Source/Extensions/UIColor.swift
+++ Budi/Budi/Source/Extensions/UIColor.swift
@@ -8,6 +8,70 @@
 import UIKit
 
 extension UIColor {
+    // MARK: - Primary
+    static var primary: UIColor {
+        UIColor(red: 94/255, green: 95/255, blue: 219/255, alpha: 1)
+    }
+    static var primarySub: UIColor {
+        UIColor(red: 236/255, green: 237/255, blue: 250/255, alpha: 1)
+    }
+    static var primaryVariant: UIColor {
+        UIColor(red: 67/255, green: 68/255, blue: 191/255, alpha: 1)
+    }
+    
+    // MARK: - Text
+    static var textHigh: UIColor {
+        UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
+    }
+    static var textMedium: UIColor {
+        UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1)
+    }
+    static var textDisabled: UIColor {
+        UIColor(red: 158/255, green: 158/255, blue: 158/255, alpha: 1)
+    }
+    
+    // MARK: - Color
+    static var border: UIColor {
+        UIColor(red: 224/255, green: 224/255, blue: 224/255, alpha: 1)
+    }
+    static var white: UIColor {
+        UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    }
+    static var background: UIColor {
+        UIColor(red: 241/255, green: 241/255, blue: 241/255, alpha: 1)
+    }
+    
+    // MARK: - Status
+    static var warning: UIColor {
+        UIColor(red: 218/255, green: 30/255, blue: 40/255, alpha: 1)
+    }
+    static var ok: UIColor {
+        UIColor(red: 0/255, green: 117/255, blue: 255/255, alpha: 1)
+    }
+
+    // MARK: - Job
+    static var jobDeV: UIColor {
+        UIColor(red: 51/255, green: 130/255, blue: 224/255, alpha: 1)
+    }
+    static var jobDevSub: UIColor {
+        UIColor(red: 231/255, green: 241/255, blue: 251/255, alpha: 1)
+    }
+    static var jobDesign: UIColor {
+        UIColor(red: 233/255, green: 98/255, blue: 98/255, alpha: 1)
+    }
+    static var jobDesignSub: UIColor {
+        UIColor(red: 254/255, green: 237/255, blue: 237/255, alpha: 1)
+    }
+    static var jobPlan: UIColor {
+        UIColor(red: 64/255, green: 193/255, blue: 162/255, alpha: 1)
+    }
+    static var jobPlanSub: UIColor {
+        UIColor(red: 232/255, green: 248/255, blue: 245/255, alpha: 1)
+    }
+}
+
+// MARK: - Old Color
+extension UIColor {
     static var budiGreen: UIColor {
         UIColor(red: 0.41, green: 0.86, blue: 0.67, alpha: 1)
     }

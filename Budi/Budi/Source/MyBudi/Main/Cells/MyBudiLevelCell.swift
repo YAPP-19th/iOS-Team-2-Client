7ee778f1921184c9ccf77c04d20355f9b9124fbf
ITlearning <yo7504@naver.com>
2022-01-09 12:42:52 +0900

    #56 - 코드 최신화

--- Budi/Budi/Source/MyBudi/Main/Cells/MyBudiLevelCell.swift
+++ Budi/Budi/Source/MyBudi/Main/Cells/MyBudiLevelCell.swift
@@ -6,11 +6,39 @@
 //
 
 import UIKit
+import Combine
+import CombineCocoa
 
 final class MyBudiLevelCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    @IBOutlet weak var levelSlider: CustomSlider!
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        configureLayout()
     }
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureLayout()
+    }
+
+    private func configureLayout() {
+        let thumbImage = UIImage(named: "Dev_Lv2")
+        levelSlider.setThumbImage(thumbImage, for: .normal)
+        levelSlider.setThumbImage(thumbImage, for: .selected)
+    }
+
+    func setLevel(level: String) {
+        
+    }
+
+    private func thumbImage() -> UIImage {
+        let thumbView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        thumbView.backgroundColor = .clear
+        let render = UIGraphicsImageRenderer(bounds: thumbView.bounds)
+        return render.image { context in
+            thumbView.layer.render(in: context.cgContext)
+
+        }
+    }
 }
---
+++ Budi/Budi/Source/MyBudi/Main/UserLevel.swift
@@ -0,0 +1,16 @@
+//
+//  UserLevel.swift
+//  Budi
+//
+//  Created by 인병윤 on 2022/01/08.
+//
+
+import Foundation
+
+
+enum UserLevel {
+    case levelOne
+    case levelTwo
+    case levelThree
+    case levelFour
+}

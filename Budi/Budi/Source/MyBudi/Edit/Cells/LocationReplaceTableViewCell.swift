4fd02d0b19ca57e93435a4a9934a6ebfe8680a19
ITlearning <yo7504@naver.com>
2022-01-10 22:42:34 +0900

    #61 - 테이블뷰를 이용한 셀 구성

---
+++ Budi/Budi/Source/MyBudi/Edit/Cells/LocationReplaceTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PositionTableViewCell.swift
+//  Budi
+//
+//  Created by 인병윤 on 2022/01/10.
+//
+
+import UIKit
+
+class LocationReplaceTableViewCell: UITableViewCell {
+
+    static let cellId = "LocationReplaceTableViewCell"
+
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var replaceLocationButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Budi/Budi/Source/MyBudi/Edit/Cells/NormalTextFieldTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  NormalTextFieldTableViewCell.swift
+//  Budi
+//
+//  Created by 인병윤 on 2022/01/10.
+//
+
+import UIKit
+
+class NormalTextFieldTableViewCell: UITableViewCell {
+
+    static let cellId = "NormalTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
